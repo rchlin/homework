5abe8d5d66e9f24a92bf3f07f155c426dd6a6952
TeacherYang <ruanchenglin@aliyun.com>
2014-11-16 11:03:16 +0800

    rails new homework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homework::Application.config.secret_token = 'fc2a2afa6634db3872e94dbeafda48565dc6cb5dc5fb1b70dd45f574e8c43a65aa0ce8dae505bc6a775930d07e671fe4134f370480acc8cb72ba360656663d2e'
